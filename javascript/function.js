5e599b601c99df6d577dc91daf19845a9007c098
Harsh Kumar Rawani <harshrawani2004@gmail.com>
2025-07-31 12:28:25 +0000

    JS function and print function 'console. log()' complete

---
+++ javascript/function.js
@@ -0,0 +1,12 @@
+function printer(){
+    let age = 22;
+    console.log("my name is Harsh. My age is:", age);
+}
+
+function printer2() {
+  let age = 22;
+  console.log(`my name is Harsh. I am ${age} years old`);
+}
+
+printer();
+printer2();
