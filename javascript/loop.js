fa966d2cb22048771601755094227bb89cd01020
Harsh Kumar Rawani <harshrawani2004@gmail.com>
2025-07-31 12:30:49 +0000

    JS loop syntax

---
+++ javascript/loop.js
@@ -0,0 +1,22 @@
+let teas = ["masala", "ginger", "oolong", "orange", "rose", "lemon"];
+
+
+teas.length;
+
+
+for (let h = 0; h < teas.length; h++) {
+  console.log(`tea at index ${h}: ${teas[h]}`);
+}
+
+
+let p;
+let num =3
+if (num == 2) {
+  p = "two";
+} else if (num == 3) {
+  p = "three";
+} else {
+  p = "don`t know";
+}
+
+console.log(p);
