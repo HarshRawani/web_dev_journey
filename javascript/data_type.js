7367f46453515064ed572c922cc57918f080c3af
Harsh Kumar Rawani <harshrawani2004@gmail.com>
2025-07-31 12:29:18 +0000

    JS function Data Types topic complete

---
+++ javascript/data_type.js
@@ -0,0 +1,32 @@
+// string - "", '', ``
+let name = "Rohit";
+// Number - 25, 25.9
+let age = 25;
+
+// Boolean - true, false
+let isPaid = true;
+
+// undefined and null
+let favoriteClass = null;
+let hometown;
+
+console.log(favoriteClass);
+console.log(hometown);
+
+// array
+let skills = ["html", "CSS", "Js"];
+
+// object
+let studentProfile = {
+  name: "Harsh",
+  age: 22,
+  isPaid: true,
+  skills: ["HTML", "CSS", "JS"],
+  favoriteClass: null,
+};
+
+console.log(typeof isPaid);
+console.log(typeof skills);
+console.log(typeof studentProfile);
+console.log(typeof favoriteClass);
+console.log(typeof hometown);
